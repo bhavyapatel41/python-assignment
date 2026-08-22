6f98339d816c69d8a4060d4575908794fde3bda3
bhavyapatel41 <bhavya.paresh.patel.cg@gmail.com>
2026-08-22 20:08:01 +0530

    file variable added

---
+++ 02.variable/1.py
@@ -0,0 +1,46 @@
+#variable assi
+
+#24
+name="bhavya"
+age=19
+city='vadodara'
+print("name : ",name,"\nage : ",age,"\ncity : ",city)
+
+#25
+student_name="kashvi"
+student_rollNo=18
+student_branch="""CSE"""
+print("name : ",student_name,"\nstudent rollno: ",student_rollNo,"\nstudent branch : ",student_branch)
+
+#26
+marks=100
+marks=99
+print(marks)
+
+#27
+
+name2="rahul"
+age2=19
+city2="patna"
+print(name2,age2,city2)
+
+#28
+x=y=z=0
+print(x,y,z)
+
+#29
+
+student1="anushka"
+student_Name="shrusti"
+class_name="B.Tech"
+
+print(student1,student_Name,class_name)
+
+#30
+
+#student information
+
+Student_Name="vaishvi"
+Student_age=16
+Student_age=18
+print(Student_age,Student_Name)
