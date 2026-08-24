d513c36707f2afd0413e4d51b55ae0741c273c53
bhavyapatel41 <bhavya.paresh.patel.cg@gmail.com>
2026-08-24 16:39:44 +0530

    file dataTypes added

---
+++ 03.dataTypes/1.py
@@ -0,0 +1,12 @@
+age=22
+int_to_string=str(age)
+int_to_float=float(age)
+a=None
+print(type(22),type(a),a)
+print(type(int_to_string),type(int_to_float),int_to_float,int_to_string)
+
+
+
+Age=29
+Age=str(Age)
+print(Age,type(Age))
