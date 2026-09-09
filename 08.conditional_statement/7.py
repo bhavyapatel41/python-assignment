574d201deca23346e0d5da65af938e14c207d9f4
bhavyapatel41 <bhavya.paresh.patel.cg@gmail.com>
2026-09-09 09:43:19 +0530

    assignment 2 of conditional statement added

--- 08.conditional_statement/7.py
+++ 08.conditional_statement/7.py
@@ -1,104 +1,104 @@
-# # #LEVEL--2
+#LEVEL--2
 
-# # #11
-# # year=int(input("enter year : "))
+#11
+year=int(input("enter year : "))
 
-# # if year%400==0 or year%4==0 and not year%4==0:
-# #     print("leap year")
-# # else:
-# #     print("not a leap year")
+if year%400==0 or year%4==0 and not year%4==0:
+    print("leap year")
+else:
+    print("not a leap year")
 
-# # #12
-# # character=input("Enter character : ")
+#12
+character=input("Enter character : ")
 
-# # if character>='A' and character<='Z':
-# #     print(" Uppercase alphabet")
-# # elif character>='a' and character<='z':
-# #     print("lowercase")
-# # elif character>='0' and character<='9':
-# #     print("digit")
-# # else:
-# #     print("special character")
+if character>='A' and character<='Z':
+    print(" Uppercase alphabet")
+elif character>='a' and character<='z':
+    print("lowercase")
+elif character>='0' and character<='9':
+    print("digit")
+else:
+    print("special character")
 
-# # #13
-# # character1=input("Enter character : ")
+#13
+character1=input("Enter character : ")
 
-# # if character1 in "aeiouAEIOU":
-# #     print("vowels")
-# # elif character1>="0" and character1<="9":
-# #     print("digit")
-# # else:
-# #     print("consonants")
+if character1 in "aeiouAEIOU":
+    print("vowels")
+elif character1>="0" and character1<="9":
+    print("digit")
+else:
+    print("consonants")
 
 
-# # #14 and 15
+#14 and 15
 
-# # cost_price=float(input("Enter cost price : "))
-# # sell_price=float(input("Enter sell price : "))
+cost_price=float(input("Enter cost price : "))
+sell_price=float(input("Enter sell price : "))
 
-# # profit=sell_price-cost_price
-# # loss=cost_price-sell_price
+profit=sell_price-cost_price
+loss=cost_price-sell_price
 
-# # if cost_price<sell_price:
-# #     print(f"profit : {(profit/cost_price)*100}")
-# # elif cost_price>sell_price:
-# #     print(f"loss {(loss/cost_price)*100}")
-# # else:
-# #     print("equal")
+if cost_price<sell_price:
+    print(f"profit : {(profit/cost_price)*100}")
+elif cost_price>sell_price:
+    print(f"loss {(loss/cost_price)*100}")
+else:
+    print("equal")
 
-# # #16
+#16
 
-# # units=int(input("Enter electricity bill units: "))
+units=int(input("Enter electricity bill units: "))
 
-# # if units>=0 and units<=100:
-# #     print(f"bill : {5*units}")
-# # elif units>100 and units<=200:
-# #     print(f"bill : {7*units}")
-# # else:
-# #     print(f"bill : {10*units}")
+if units>=0 and units<=100:
+    print(f"bill : {5*units}")
+elif units>100 and units<=200:
+    print(f"bill : {7*units}")
+else:
+    print(f"bill : {10*units}")
 
-# #17
-# num1=int(input("Enter num 1 : "))
-# num2=int(input("Enter num 2 : "))
-# operator=input("Enter operator : ")
+#17
+num1=int(input("Enter num 1 : "))
+num2=int(input("Enter num 2 : "))
+operator=input("Enter operator : ")
 
-# if operator=="+":
-#     print("addition : ",num1+num2)
-# elif operator=="-":
-#     print(f"substraction : {num1-num2}")
-# elif operator=="/":
-#     if(num2==0):
-#         print("ZeroError")
-#     else:
-#         print(f"division : {num1/num2}")
-# else:
-#     print(f"multiplication : {num1*num2}")
+if operator=="+":
+    print("addition : ",num1+num2)
+elif operator=="-":
+    print(f"substraction : {num1-num2}")
+elif operator=="/":
+    if(num2==0):
+        print("ZeroError")
+    else:
+        print(f"division : {num1/num2}")
+else:
+    print(f"multiplication : {num1*num2}")
 
-# #18
-# temp=float(input("Enter temperature : "))
+#18
+temp=float(input("Enter temperature : "))
 
-# if temp<0:
-#     print("Freezing")
-# elif temp>=0 and temp<=15:
-#     print("very cold")
-# elif temp>15 and temp<=25:
-#     print("cold")
-# elif temp>=26 and temp<35:
-#     print("normal")
-# else:
-#     print("Hot")
+if temp<0:
+    print("Freezing")
+elif temp>=0 and temp<=15:
+    print("very cold")
+elif temp>15 and temp<=25:
+    print("cold")
+elif temp>=26 and temp<35:
+    print("normal")
+else:
+    print("Hot")
 
-# #19
-# number=int(input("Enter number : "))
+#19
+number=int(input("Enter number : "))
 
-# if number<0:
-#     print("negative")
-# elif number>=0 and number<=50:
-#     print("Number is between 0 to 50")
-# elif number>=51 and number<=100:
-#     print("Number is between 51 to 100")
-# else:
-#     print("above 100")
+if number<0:
+    print("negative")
+elif number>=0 and number<=50:
+    print("Number is between 0 to 50")
+elif number>=51 and number<=100:
+    print("Number is between 51 to 100")
+else:
+    print("above 100")
 
 #20
 a=int(input("Enter a : "))
--- 08.conditional_statement/8.py
+++ 08.conditional_statement/8.py
@@ -1,144 +1,144 @@
 #LEVEL--3
 
-# #21
-# a=int(input("Enter a : "))
-# b=int(input("Enter b : "))
-# c=int(input("Enter c : "))
+#21
+a=int(input("Enter a : "))
+b=int(input("Enter b : "))
+c=int(input("Enter c : "))
 
-# if a+b>c and a+c>b and b+c>a:
-#     print(" valid triangle")
-#     if a==b==c:
-#         print(" Equilateral: all three sides equal")
-#     elif a==b or b==c or c==a:
-#         print("Isosceles : exactly two sides equal")
-#     else:
-#         print("Scalene: all sides different")
+if a+b>c and a+c>b and b+c>a:
+    print(" valid triangle")
+    if a==b==c:
+        print(" Equilateral: all three sides equal")
+    elif a==b or b==c or c==a:
+        print("Isosceles : exactly two sides equal")
+    else:
+        print("Scalene: all sides different")
     
-# else:
-#     print("invalid triangle")
+else:
+    print("invalid triangle")
 
 #22
 
-# account_balance=float(input("Enter balance : "))
-# withdrawal_amount=float(input("Enter amount : "))
+account_balance=float(input("Enter balance : "))
+withdrawal_amount=float(input("Enter amount : "))
 
-# if withdrawal_amount<(account_balance+500) and withdrawal_amount%100==0 and withdrawal_amount>0:
-#     print("Withdrwal successful\n")
-#     print(f"Remaining balance : {account_balance-withdrawal_amount}")
-# else:
-#     print("invalid amount")
+if withdrawal_amount<(account_balance+500) and withdrawal_amount%100==0 and withdrawal_amount>0:
+    print("Withdrwal successful\n")
+    print(f"Remaining balance : {account_balance-withdrawal_amount}")
+else:
+    print("invalid amount")
 
-# if withdrawal_amount>0:
-#     if withdrawal_amount<(account_balance-500):
-#         if withdrawal_amount%100==0:
-#             print("Withdrwal successful\n")
-#             print(f"Remaining balance : {account_balance-withdrawal_amount}")
-#         else:
-#             print("invalid amount")
-#     else:
-#         print("insufficient balance to withdraw money")
-# else:
-#     print("amount should be grater then zero")
+if withdrawal_amount>0:
+    if withdrawal_amount<(account_balance-500):
+        if withdrawal_amount%100==0:
+            print("Withdrwal successful\n")
+            print(f"Remaining balance : {account_balance-withdrawal_amount}")
+        else:
+            print("invalid amount")
+    else:
+        print("insufficient balance to withdraw money")
+else:
+    print("amount should be grater then zero")
 
 #23
 
-# username=input("Enter username : ")
-# password=input("Enter password : ")
-# if username!="admin":
-#     print("User not found")
-# elif password!="python123":
-#         print("Wrong password")
-# else:
-#         print("login succesful")    
+username=input("Enter username : ")
+password=input("Enter password : ")
+if username!="admin":
+    print("User not found")
+elif password!="python123":
+        print("Wrong password")
+else:
+        print("login succesful")    
 
 #24
 
-# purchase_amount=float(input("Enter purchase amount : "))
-# print("Purchase: ",purchase_amount)
-# discount_amount=0
-# if purchase_amount<500:
-#        print("Discount : 0%")
-#        discount_amount=purchase_amount
-# elif purchase_amount>=500 and purchase_amount<=999:
-#        print("Discount : 5%")
-#        discount_amount=purchase_amount*0.05
-# elif purchase_amount>=1000 and purchase_amount<=1999:
-#        print("Discount : 10%")
-#        discount_amount=purchase_amount*0.1
-# elif purchase_amount>=2000 and purchase_amount<=4999:
-#        print("Discount : 15%")
-#        discount_amount=purchase_amount*0.15
-# else:
-#        print("Discount : 20%")
-#        discount_amount=purchase_amount*0.2
+purchase_amount=float(input("Enter purchase amount : "))
+print("Purchase: ",purchase_amount)
+discount_amount=0
+if purchase_amount<500:
+       print("Discount : 0%")
+       discount_amount=purchase_amount
+elif purchase_amount>=500 and purchase_amount<=999:
+       print("Discount : 5%")
+       discount_amount=purchase_amount*0.05
+elif purchase_amount>=1000 and purchase_amount<=1999:
+       print("Discount : 10%")
+       discount_amount=purchase_amount*0.1
+elif purchase_amount>=2000 and purchase_amount<=4999:
+       print("Discount : 15%")
+       discount_amount=purchase_amount*0.15
+else:
+       print("Discount : 20%")
+       discount_amount=purchase_amount*0.2
 
-# print(f"discount amount: {discount_amount}")
-# print(f"final amount : {purchase_amount-discount_amount}")
+print(f"discount amount: {discount_amount}")
+print(f"final amount : {purchase_amount-discount_amount}")
 
 #25
-# m1=int(input("Enter a : "))
-# m2=int(input("Enter b : "))
-# m3=int(input("Enter c : "))
-# avg=(m1+m2+m3)/3
-# if m1>=35 and m2>=35 and m3>35:
+m1=int(input("Enter a : "))
+m2=int(input("Enter b : "))
+m3=int(input("Enter c : "))
+avg=(m1+m2+m3)/3
+if m1>=35 and m2>=35 and m3>35:
        
-#     if avg>=75:
-#         print("Distinction")
-#     elif avg>=60 and avg<=74:
-#        print("first class")
-#     elif avg>=50 and avg<59:
-#         print("second class")
-#     else:
-#         print("pass")
-# else:
-#      print("fail")
+    if avg>=75:
+        print("Distinction")
+    elif avg>=60 and avg<=74:
+       print("first class")
+    elif avg>=50 and avg<59:
+        print("second class")
+    else:
+        print("pass")
+else:
+     print("fail")
     
 #26
 
-# day=int(input("Enter day : "))
-# month=int(input("Enter month : "))
-# year=int(input("Enter year : "))
+day=int(input("Enter day : "))
+month=int(input("Enter month : "))
+year=int(input("Enter year : "))
 
 # if month>=1 and month<=12:
 #     if month%2==0:
 
 
 #27
-# hours=int(input("Enter hours : "))
-# min=int(input("Enter min : "))
-# sec=int(input("Enter sec : "))
+hours=int(input("Enter hours : "))
+min=int(input("Enter min : "))
+sec=int(input("Enter sec : "))
 
-# if hours>=0 and hours<=23:
-#     if min>=0 and min<=59:
-#         if sec>=0 and sec<=59:
-#             print("valid time")
-# else:
-#     print("invalid time")            
+if hours>=0 and hours<=23:
+    if min>=0 and min<=59:
+        if sec>=0 and sec<=59:
+            print("valid time")
+else:
+    print("invalid time")            
 
 #28
 
-# person1=input("enter name 1: ")
-# age1=input("enter age 1 : ")
-# person2=input("enter name 2: ")
-# age2=input("enter age 2 : ")
-# person3=input("enter name 3: ")
-# age3=input("enter age 3 : ")
+person1=input("enter name 1: ")
+age1=input("enter age 1 : ")
+person2=input("enter name 2: ")
+age2=input("enter age 2 : ")
+person3=input("enter name 3: ")
+age3=input("enter age 3 : ")
 
-# if age1>age2 and age1>age3:
-#     print(f"{person1} is youngest")
-# elif age2>age1 and age2>age3:
-#      print(f"{person2} is youngest")
-# elif age3>age1 and age3>age2:
-#      print(f"{person3} is youngest")
-# elif age1==age2 or age2==age3 or age3==age2:
-#      print("2 people have same age")
-# else:
-#      print("All three have the same age")
+if age1>age2 and age1>age3:
+    print(f"{person1} is youngest")
+elif age2>age1 and age2>age3:
+     print(f"{person2} is youngest")
+elif age3>age1 and age3>age2:
+     print(f"{person3} is youngest")
+elif age1==age2 or age2==age3 or age3==age2:
+     print("2 people have same age")
+else:
+     print("All three have the same age")
 
-#29
-# num1=int(input("Enter num1 : "))
-# num2=int(input("Enter num2 : "))
-# num3=int(input("Enter num3 : "))
+29
+num1=int(input("Enter num1 : "))
+num2=int(input("Enter num2 : "))
+num3=int(input("Enter num3 : "))
 
 #30
 student_age=int(input("Enter age : "))
