224d22b3e3a554c49bb64b19388cbf4ae894bbf2
bhavyapatel41 <bhavya.paresh.patel.cg@gmail.com>
2026-09-12 10:26:34 +0530

    for loops assignment

---
+++ 09.loops/6.py
@@ -0,0 +1,33 @@
+#26
+for i in range(3):
+    for j in range(4):
+        print("*",end="")
+    print()
+print("\n")
+#27
+for i in range(4):
+    for j in range(5):
+        print("*",end="")
+    print()
+print("\n")
+
+#28
+for i in range(5):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+
+print("\n")
+
+#29
+for i in range(6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+print("\n")
+
+#30
+for i in range(1,6):
+    for j in range(1,11):
+        print(i*j,end=" ")
+    print()    
